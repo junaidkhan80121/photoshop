0391f7f53e0590e9a3a046bd7d230a0eb458595d
Junaid Khan <khanjunaid80121@gmail.com>
2025-02-06 14:54:19 +0530

    FINAL UPDATE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import { ThemeProvider } from "@emotion/react";
-import { createTheme } from "@mui/material";
+import {
+  Button,
+  createTheme,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@mui/material";
 import { useCallback, useState } from "react";
 import NavBar from "./Navbar";
 import ImageCropper from "./Cropper";
 
 function App() {
+  const [dialog, setDialog] = useState(true);
   const checkThemeMode = useCallback(() => {
     let mode = localStorage.getItem("darkMode");
     if (!mode) {
@@ -22,10 +31,37 @@ function App() {
 
   return (
     <>
-    <ThemeProvider theme={theme}>
-      <NavBar darkMode={darkMode} setdarkMode={setdarkMode} />
-      <ImageCropper darkMode={darkMode} />
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Dialog
+          open={dialog}
+          onClose={() => {
+            setDialog(false);
+          }}
+        >
+          <DialogTitle>
+            <Typography variant="h5">Please Note</Typography>
+          </DialogTitle>
+          <DialogContent>
+            <Typography>
+              Due to hosting limitations, the app may take a little while to load for
+              the first time to become available. Thanks for your patience!
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => {
+                setDialog(false);
+              }}
+            >
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <NavBar darkMode={darkMode} setdarkMode={setdarkMode} />
+        <ImageCropper darkMode={darkMode} />
+      </ThemeProvider>
     </>
   );
 }
